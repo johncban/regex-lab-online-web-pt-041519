4d0a9218a7099d04d25471acd2b9377507ec983b
johncban <johncban@gmail.com>
2019-05-25 06:10:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,12 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\A[A-Z]\w*\W\b/) != nil
+  p = /\A[A-Z]\w*\W\b/
+  if text.match(p) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def valid_phone_number?(phone)
